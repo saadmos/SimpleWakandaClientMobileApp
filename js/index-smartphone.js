34e9a7b0ba0fec905757aec042ec26336382dd3c
Saad Mousliki <saadmos06@gmail.com>
2013-01-09 17:49:49 +0000

    4
    4

---
+++ js/index-smartphone.js
@@ -0,0 +1,26 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		$$('navigationView1').goToNextView();
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		array = [{company : "company1"}, {company : "company2"}, {company : "company3"}, {company : "company4"}, {company : "company5"}];
+		sources.array.sync();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
